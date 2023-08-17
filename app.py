eeb12f3b16f58a3bf7362e6f0a8a2e37a4dba1f7
h3samayoa <h3samayoa@gmail.com>
2023-08-17 10:01:57 -0700

    switching to artifact

--- app.py
+++ app.py
@@ -2,5 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+
 def hello():
     return 'container works'
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup 
+
+setup(
+    name='flask_app',
+    version='0.0.1',
+    packages=['flask_app'],
+)
