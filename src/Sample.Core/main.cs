3ac9982bb4fe383a8828e89889416b53ccb80fe7
h3samayoa <h3samayoa@gmail.com>
2023-09-19 15:10:44 -0700

    remove lambda

--- src/Sample.Core/main.cs
+++ src/Sample.Core/main.cs
@@ -1,15 +1,16 @@
+using System;
 namespace Sample.Core;
 
 public static class FizzBuzz
 {
   public static string ToFizzBuzzFormat(int i)
-    => i % 15 == 0 ? "Fizz Buzz"
-    : i % 5 == 0 ? "Buzz"
-    : i % 3 == 0 ? "Fizz"
-    : i.ToString();
-
-  static void Main(string[] args)
   {
+    if (i % 3 == 0 && i % 5 == 0) {
+      Console.WriteLine("FizzBuzz")
+    }
+  }
+
+  static void Main(string[] args) {
     ToFizzBuzzFormat(5);
   }
 
