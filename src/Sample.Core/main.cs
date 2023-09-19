ebbcdb81fefe26a3907988be551a128a2cb1a48f
h3samayoa <h3samayoa@gmail.com>
2023-09-19 15:04:38 -0700

    remove brackets

--- src/Sample.Core/main.cs
+++ src/Sample.Core/main.cs
@@ -3,14 +3,13 @@ namespace Sample.Core;
 public static class FizzBuzz
 {
   public static string ToFizzBuzzFormat(int i)
-  {
     => i % 15 == 0 ? "Fizz Buzz"
     : i % 5 == 0 ? "Buzz"
-    : i % 3 == 0? "Fizz"
+    : i % 3 == 0 ? "Fizz"
     : i.ToString();
-  }
 
-  static void Main(string[] args) {
+  static void Main(string[] args)
+  {
     ToFizzBuzzFormat(5);
   }
 
